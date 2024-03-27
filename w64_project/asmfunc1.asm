cd24e98127a7340c82bcce6164c530e230c91b09
izzywincy <izabella_imperial@dlsu.edu.ph>
2024-03-27 17:24:46 +0800

    Add project files.

---
+++ w64_project/asmfunc1.asm
@@ -0,0 +1,12 @@
+; x86_64 assembly program
+
+section .text
+bits 64
+default rel
+
+global dotproduct
+
+dotproduct:
+
+
+	ret
---
+++ w64_project/main.c
@@ -0,0 +1,14 @@
+// x86_64 floating point caller program
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+extern double dotproduct();
+
+int main() {
+
+
+
+	return 0;
+}
