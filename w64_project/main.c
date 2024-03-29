93dba325f2947ba491a61b4ffddb7f7a3f0b8e39
david-estrera <90037135+david-estrera@users.noreply.github.com>
2024-03-30 06:21:31 +0800

    Update main.c

--- w64_project/main.c
+++ w64_project/main.c
@@ -1,32 +1,35 @@
-// x86_64 floating point caller program
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 
-extern double dotproduct(float* A, float* B, double sdot);
+extern double dotproduct(float* A, float* B);
 
 int main() {
-	int n;
-	printf("Enter length of the vector: ");
-	scanf_s("%d", &n);
-	float* A = (float*)malloc(n * sizeof(float));
-	float* B = (float*)malloc(n * sizeof(float));
+    int n;
+    printf("Enter length of the vector: ");
+    scanf_s("%d", &n);
+    float* A = (float*)malloc(n * sizeof(float));
+    float* B = (float*)malloc(n * sizeof(float));
 
-	printf("Enter Vector A: ");
-	for (int i = 0; i < n; i++) {
-		scanf_s("%f", &A[i]);
-	}
+    printf("Enter Vector A: ");
+    for (int i = 0; i < n; i++) {
+        scanf_s("%f", &A[i]);
+    }
 
-	printf("Enter Vector B: ");
-	for (int i = 0; i < n; i++) {
-		scanf_s("%f", &B[i]);
-	}
+    printf("Enter Vector B: ");
+    for (int i = 0; i < n; i++) {
+        scanf_s("%f", &B[i]);
+    }
 
-	for (int i = 0; i < n; i++) {
-		printf("%f %f", A[i], B[i]);
-	}
+    for (int i = 0; i < n; i++) {
+        printf("%f %f\n", A[i], B[i]);
+    }
 
-	double sdot = dotproduct(&A, &B, sdot)
-	return 0;
+    double sdot = dotproduct(A, B);
+    printf("Dot product: %lf\n", sdot);
+
+    // Free allocated memory
+    free(A);
+    free(B);
+
+    return 0;
 }
