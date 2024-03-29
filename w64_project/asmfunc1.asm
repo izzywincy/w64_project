f846029187eb4dd87b086d3eee0000a4e072c888
david-estrera <90037135+david-estrera@users.noreply.github.com>
2024-03-30 06:24:26 +0800

    Update asmfunc1.asm
    not  yet  fully working,  will debug  later

--- w64_project/asmfunc1.asm
+++ w64_project/asmfunc1.asm
@@ -7,6 +7,36 @@ default rel
 global dotproduct
 
 dotproduct:
+    push rbp            ; Function prologue
+    mov rbp, rsp
+
+    ; Initialize accumulator (result) to 0
+    xorps xmm0, xmm0
+
+    ; Load the length of the vectors into ecx
+    mov ecx, dword [rdi]
+
+dotproduct_loop:
+    ; Compare loop counter with vector length
+    cmp ecx, 0
+    je dotproduct_end   ; If vector length is 0, exit loop
+
+    ; Load A[i] into xmm1
+    movss xmm4, dword [rbp + 32]
 
 
-	ret
+    ; Load B[i] into xmm2
+    movss xmm4, dword [rbp + 40]
+
+
+    ; Multiply A[i] by B[i] and accumulate the result
+    mulss xmm1, xmm2
+    addss xmm0, xmm1
+
+    ; Decrement loop counter
+    dec ecx
+    jmp dotproduct_loop ; Jump to the beginning of the loop
+
+dotproduct_end:
+    pop rbp             ; Function epilogue
+    ret
