a38eaab5f360791c93638e44985f120e220434e4
david-estrera <90037135+david-estrera@users.noreply.github.com>
2024-03-31 17:48:43 +0800

    Update asmfunc1.asm

--- w64_project/asmfunc1.asm
+++ w64_project/asmfunc1.asm
@@ -1,42 +1,18 @@
-; x86_64 assembly program
-
 section .text
 bits 64
 default rel
-
 global dotproduct
 
 dotproduct:
-    push rbp            ; Function prologue
-    mov rbp, rsp
 
-    ; Initialize accumulator (result) to 0
-    xorps xmm0, xmm0
+	MOV	R11, 0
+	dotloop:
+	MOVSS XMM1, dword[RDX+R11*4]
+	MOVSS XMM2, dword[R8+R11*4]
+	MULSS XMM1, XMM2
+	ADDSS XMM0, XMM1
+	INC R11
+	CMP R11, R9
+	JNE dotloop
 
-    ; Load the length of the vectors into ecx
-    mov ecx, dword [rdi]
-
-dotproduct_loop:
-    ; Compare loop counter with vector length
-    cmp ecx, 0
-    je dotproduct_end   ; If vector length is 0, exit loop
-
-    ; Load A[i] into xmm1
-    movss xmm4, dword [rbp + 32]
-
-
-    ; Load B[i] into xmm2
-    movss xmm4, dword [rbp + 40]
-
-
-    ; Multiply A[i] by B[i] and accumulate the result
-    mulss xmm1, xmm2
-    addss xmm0, xmm1
-
-    ; Decrement loop counter
-    dec ecx
-    jmp dotproduct_loop ; Jump to the beginning of the loop
-
-dotproduct_end:
-    pop rbp             ; Function epilogue
-    ret
+	ret
