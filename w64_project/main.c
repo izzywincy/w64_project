0925265644ac4b9e18205788dbf71f7b9b9a2a61
izzywincy <izabella_imperial@dlsu.edu.ph>
2024-04-01 19:13:52 +0800

    fixed errors

--- w64_project/main.c
+++ w64_project/main.c
@@ -2,21 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
+#include <math.h>
 
 extern float dotproduct(float sdot, float* A, float* B, int n);
 
-float dot_product(float sdot, float* A, float* B, int n) {
-    sdot = 0;
+float dot_product(float* A, float* B, int n) {
+    float sdot = 0;
     for (int i = 0; i < n; i++) {
         sdot += A[i] * B[i];
     }
     return sdot;
 }
 
-// Function to generate a random float between min and max
-float randomFloat(float min, float max) {
-    return min + (float)rand() / ((float)RAND_MAX / max);
-}
+
 
 int main() {
     clock_t start_time, end_time;
@@ -26,6 +24,7 @@ int main() {
     printf("Enter length of the vector: ");
     scanf_s("%d", &n);
 
+    n = pow(2, n);
     // Seed the random number generator with current time
     srand((unsigned int)time(NULL));
 
@@ -34,30 +33,20 @@ int main() {
 
     // Generate random numbers and store them in the arrays
     for (int i = 0; i < n; i++) {
-        A[i] = randomFloat(0.0, 100.0); // Random float between 0.0 and 100.0
-        B[i] = randomFloat(0.0, 100.0); // Random float between 0.0 and 100.0
-    }
-    /*
-    printf("Enter Vector A: ");
-    for (int i = 0; i < n; i++) {
-        scanf_s("%f", &A[i]);
+        A[i] = (float)rand() / RAND_MAX;; 
+        B[i] = (float)rand() / RAND_MAX;; 
     }
 
-    printf("Enter Vector B: ");
-    for (int i = 0; i < n; i++) {
-        scanf_s("%f", &B[i]);
-    }
-    */
 
-    
+  /*
     for (int i = 0; i < n; i++) {
         printf("%f %f = %f\n", A[i], B[i], A[i] * B[i]);
-    }
+    }*/
     
 
-    float sdot = 0;
+    float sdot = 0.0 ;
 
-    printf("\nx86_64\n");
+    printf("\n- x86_64");
 
     // Record the start time
     start_time = clock();
@@ -66,24 +55,24 @@ int main() {
     // Record the end time
     end_time = clock();
 
-    printf("\nDot product: %f\n", sdot);
+    printf("\nDot product: %f", sdot);
 
     // Calculate the CPU time used
     cpu_time_used = (double)(end_time - start_time) / CLOCKS_PER_SEC;
 
-    printf("\nCPU time used by x86-64 with vector length %d: %f seconds\n",n, cpu_time_used);
+    printf("\nCPU time used with vector length %d: %f seconds",n, cpu_time_used);
 
-    sdot = 0;
+    sdot = 0.0;
 
-    printf("\n\nC program\n");
+    printf("\n- C program");
 
     start_time = clock();
     // Get sdot
-    sdot = dot_product(sdot, A, B, n);
+    sdot = dot_product(A, B, n);
     // Record the end time
     end_time = clock();
 
-    printf("\nDot product: %f\n", sdot);
+    printf("\nDot product (C program): %f", sdot);
 
     // Calculate the CPU time used
     cpu_time_used = (double)(end_time - start_time) / CLOCKS_PER_SEC;
